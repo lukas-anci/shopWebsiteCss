0cd6112a1544a7bc5f179ee7ff91201760c0cd97
Lukas <lukas.ancikevicius@gmail.com>
2021-09-01 14:41:08 +0300

    axios, stringTourl

--- src/components/Sections/Shop/ShopCategories.jsx
+++ src/components/Sections/Shop/ShopCategories.jsx
@@ -1,11 +1,43 @@
 import css from './ShopCategories.module.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function stringToUrl(str) {
+  let result = str.replace(' ', '-');
+  result = result.replace("'", '_');
+  return result;
+}
+stringToUrl("some's stuff");
+
 export default function ShopCategories() {
+  // 1. axios fetch
+  // 2. state
+  // 3. useEffect
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get(
+        'https://fakestoreapi.com/products/categories'
+      );
+      console.log(data);
+      setCategories(data);
+    })();
+  }, []);
   return (
     <aside className={css.cat}>
-      <a href="/shop/categories/all">All Collections</a>
-      <a href="/shop/categories/new">New Arrivals</a>
-      <div>---------</div>
-      <a href="/shop/categories/jeans">Jeans</a>
+      <a href="/shop/categories/all">
+        <strong>All Collections</strong>
+      </a>
+      <a href="/shop/categories/new">
+        <strong>New Arrivals</strong>
+      </a>
+      <div className={css.dash}></div>
+      {categories.map((name) => (
+        <a key={name} href={`/shop/categories/${stringToUrl(name)}`}>
+          {name}
+        </a>
+      ))}
     </aside>
   );
 }
