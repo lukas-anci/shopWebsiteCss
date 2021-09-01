3e3f1f4831b97a345f14a3b4edf8a66726b50a7b
Lukas <lukas.ancikevicius@gmail.com>
2021-09-01 13:51:51 +0300

    shop start

--- src/components/Sections/Shop/Shop.jsx
+++ src/components/Sections/Shop/Shop.jsx
@@ -1,9 +1,10 @@
 import ShopCategories from './ShopCategories';
 import ShopItem from './ShopItem';
+import css from './Shop.module.css';
 
 export default function Shop() {
   return (
-    <section className="container">
+    <section className={`container ${css.shop}`}>
       <ShopCategories />
       <main>
         <ShopItem />
--- src/components/Sections/Shop/ShopCategories.jsx
+++ src/components/Sections/Shop/ShopCategories.jsx
@@ -1,8 +1,9 @@
+import css from './ShopCategories.module.css';
 export default function ShopCategories() {
   return (
-    <aside>
-      <button>All Collections</button>
-      <button>New Arrivals</button>
+    <aside className={css.cat}>
+      <a href="/shop/categories/all">All Collections</a>
+      <a href="/shop/categories/new">New Arrivals</a>
       <div>---------</div>
       <a href="/shop/categories/jeans">Jeans</a>
     </aside>
